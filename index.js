b071db4502c6dba30e2526a788d62a61657035c7
prains <sleskov2013@yandex.ru>
2022-12-21 20:35:36 +0300

    123

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ let count = 0;
 let counted = [];
 
 for (let i = 0; i < blocks.length; i++) {
-    blocks[i].addEventListener('click', () => {
-        blocks[i].style.background = 'black';
-        counted.push(blocks[i]);
+    handleClick(blocks[i]);
+}
+
+function handleClick(block) {
+    block.addEventListener('click', () => {
+        block.style.background = 'black';
+        counted.push(block);
         console.log(counted);
         if (counted.length === blocks.length) {
+            setTimeout(() => {
+                for (let i = 0; i < blocks.length; i++) {
+                    handleClick(blocks[i]);
+                }
+            }, 5000)
             let temp = counted;
             counted = [];
             for (let i = 0; i < temp.length; i++) {
                 setTimeout(() => {
                     temp[i].style.background = 'white';
-                }, 2500 * i)
+                }, 1250 * i / 2)
             }
         }
-    })
+    }, { once: 'true' })
 }
